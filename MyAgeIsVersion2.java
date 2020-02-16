6f946b02bca836957cc268e4622f6e190e975699
Vincent Benesen <you@example.com>
2020-02-16 17:05:09 -0500

    MyAgeIsVersion2.java

---
+++ MyAgeIsVersion2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MyAgeIsVersion2{
+	public static void main (String [] args){
+
+	Scanner scan = new Scanner (System.in);
+	System.out.print ("State the year that you were born >");
+	int yearOfBirth = scan.nextInt();
+	int currentYear = 2020;
+	int yourAge = currentYear - yearOfBirth;
+	System.out.println ("Your age is " + yourAge);
+
+
+
+		
+	} 
+}
