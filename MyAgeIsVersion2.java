dd3384b201c70257fbba0e60c7f280dbfc64eee2
Vincent Benesen <you@example.com>
2020-02-17 19:13:58 -0500

    MyAgeIsVersion2.java

--- MyAgeIsVersion2.java
+++ MyAgeIsVersion2.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class MyAgeIsVersion2{
 	public static void main (String [] args){
 
-	Scanner scan = new Scanner (System.in);
-	System.out.print ("State the year that you were born >");
-	int yearOfBirth = scan.nextInt();
-	int currentYear = 2020;
-	int yourAge = currentYear - yearOfBirth;
-	System.out.println ("Your age is " + yourAge);
+		Scanner scan = new Scanner (System.in);
+		System.out.print ("State the year that you were born >");
+		int yearOfBirth = scan.nextInt();
+		int currentYear = 2020;
+		int yourAge = currentYear - yearOfBirth;
+		System.out.println ("Your age is " + yourAge);
 
 
 
